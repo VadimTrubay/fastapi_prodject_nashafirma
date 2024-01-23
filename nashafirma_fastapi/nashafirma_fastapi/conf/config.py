4223696a95dfc16f2dfb61cc1d64cd10c3a81335
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-23 08:50:41 +0200

    Update config.py

--- nashafirma_fastapi/nashafirma_fastapi/conf/config.py
+++ nashafirma_fastapi/nashafirma_fastapi/conf/config.py
@@ -2,19 +2,16 @@ from pydantic.v1 import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str = "database"
-    secret_key_jwt: str = "secret_key"
-    algorithm: str = "algorithm"
-
-    # mail_username: str = "user"
-    # mail_password: str = "password"
-    # mail_from: str = "mail"
-    # mail_port: int = 987654
-    # mail_server: str = "server"
-
-    redis_host: str = "localhost"
-    redis_port: int = 123456
-    redis_password: str = "weewef"
+    sqlalchemy_database_url: str
+    # secret_key: str
+    # algorithm: str
+    # mail_username: str
+    # mail_password: str
+    # mail_from: str
+    # mail_port: int
+    # mail_server: str
+    # redis_host: str
+    # redis_port: int
 
     class Config:
         env_file = ".env"
