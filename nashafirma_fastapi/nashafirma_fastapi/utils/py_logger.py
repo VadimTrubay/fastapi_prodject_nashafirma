5b24eaa23fcfaf2c296b29bd938ad6ff839adb50
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-23 08:50:14 +0200

    Update py_logger.py

--- nashafirma_fastapi/nashafirma_fastapi/utils/py_logger.py
+++ nashafirma_fastapi/nashafirma_fastapi/utils/py_logger.py
@@ -1,19 +1,17 @@
 import logging
 
-_format = (f"%(asctime)s [%(levelname)s] - %(name)s - %(funcName)s(%(lineno)d) - %(message)s")
-
-file = "./logs.log"
-
-file_handler = logging.FileHandler(file)
-file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(logging.Formatter(_format))
-
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.DEBUG)
-stream_handler.setFormatter(logging.Formatter(_format))
-
-
 def get_logger(name):
+    _format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    file = "nf_logs.log"
+
+    file_handler = logging.FileHandler(file)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(_format))
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(logging.Formatter(_format))
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(file_handler)
