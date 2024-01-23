3e083fb7c2fc3a454ad6dbab8a2318d2fa55a400
VadimTrubay <vadnetvadnet@ukr.net>
2024-01-23 08:50:39 +0200

    Update db.py

--- nashafirma_fastapi/nashafirma_fastapi/database/db.py
+++ nashafirma_fastapi/nashafirma_fastapi/database/db.py
@@ -1,15 +1,27 @@
+from fastapi import HTTPException, status
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://nashafirma:vad0101vad@localhost:5432/postgres"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
+    except SQLAlchemyError as err:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
     finally:
         db.close()
