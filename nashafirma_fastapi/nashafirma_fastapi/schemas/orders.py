b240a3de354777a33a37f9e25b3fe9328c0a2c1a
VadimTrubay <vadnetvadnet@ukr.net>
2024-02-02 09:35:31 +0200

    Update orders.py

--- nashafirma_fastapi/nashafirma_fastapi/schemas/orders.py
+++ nashafirma_fastapi/nashafirma_fastapi/schemas/orders.py
@@ -1,14 +1,23 @@
 from pydantic import BaseModel, Field
 from pydantic.types import date
 
+from nashafirma_fastapi.schemas.users import UserFromOrder
+
 
 class OrderModel(BaseModel):
     done: bool = Field(False)
 
 
-class OrderResponse(OrderModel):
+class OrderFromItem(BaseModel):
+    created_at: date
+
+    class Config:
+        from_attributes = True
+
+
+class OrderResponse(BaseModel):
     id: int
-    user: int
+    user: UserFromOrder
     created_at: date
     updated_at: date
 
